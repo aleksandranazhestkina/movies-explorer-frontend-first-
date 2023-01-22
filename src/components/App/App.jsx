8887e83bfbf02d69b94523dc191b4cd5fa1dc104
Aleksandra Nazhestkina <splinatus@yandex.ru>
2023-01-22 19:30:19 +0300

    добавить страницу логина

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,45 @@
-import './App.css';
-import Header from '../Header/Header.jsx';
-import Main from '../Main/Main.jsx';
-import Footer from '../Footer/Footer.jsx';
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import Header from "../Header/Header.jsx";
+import Main from "../Main/Main.jsx";
+import Footer from "../Footer/Footer.jsx";
+import Register from "../Register/Register.jsx";
+import Login from "../Login/Login.jsx";
+import Movies from "../Movies/Movies.jsx";
+import SavedMovies from "../SavedMovies/SavedMovies.jsx";
+import Profile from "../Profile/Profile.jsx";
+import NotFound from "../NotFound/NotFound.jsx";
+
+
 
 export default function App() {
+  const headerEndpoints = ['/movies', '/saved-movies', '/profile', '/'];
+  const footerEndpoints = ['/movies', '/saved-movies', '/'];
   return (
-    <div className='app'>
-    <Header />
-    <Main />
-    <Footer />
+    <div className="app">
+      <Route exact path={headerEndpoints}>
+      <Header />
+      </Route>
+      <Switch>
+        <Route exact path="/">
+          <Main />
+        </Route>
+        <Route exact path="/signup">
+          <Register />
+        </Route>
+        <Route exact path="/signin">
+          <Login />
+        </Route>
+        <Route path="/movies" element={Movies} />
+        <Route path="/saved-movies" element={SavedMovies} />
+        <Route path="/profile" element={Profile} />
+        <Route path="*">
+          <NotFound />
+        </Route>
+        <Route exact path={footerEndpoints}>
+        <Footer />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import './vendor/fonts/fonts.css'
 import './vendor/normalize.css';
 
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; 
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './components/App/App.jsx';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
+  document.getElementById('root')
 );
